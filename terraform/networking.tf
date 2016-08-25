44626113941a92bd26a6a81be48c0d862e9bccb5
John Williams <john@veloshots.com>
2016-08-25 00:20:50 -0500

    allow port 80 egress from the ELB

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "for-the-people-elb-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_elb" "for-the-people-web-elb" {
